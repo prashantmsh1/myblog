a62c21539867826ebfc4949f5a9e9ed9dc3dfde2
prashant <prashantkr.msh@gmail.com>
2024-07-14 01:57:41 +0530

    chore: Add initial project files and configuration

---
+++ api/index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+const app = express();
+dotenv.config();
+mongoose
+    .connect(process.env.MONGO_URL)
+    .then(() => {
+        console.log("Connected to MongoDB");
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
---
+++ client/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+import flowbite from "flowbite-react/tailwind";
+import plugin from "flowbite/plugin";
+export default {
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", flowbite.content()],
+    theme: {
+        extend: {},
+    },
+    plugins: [flowbite.plugin(), plugin],
+};
