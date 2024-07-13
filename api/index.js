673d64e11c96d06382ee3892cf11069cc754cd00
prashant <prashantkr.msh@gmail.com>
2024-07-14 02:07:34 +0530

    feat: Add user route and controller for testing
    This commit adds a new user route and controller for testing purposes. The user route is added to the API index file, and the user controller file is created with a test route that sends a "Test route" response.

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 const app = express();
+import userRoutes from "./routes/user.route.js";
 dotenv.config();
 mongoose
     .connect(process.env.MONGO_URL)
@@ -14,3 +15,5 @@ mongoose
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
 });
+
+app.use("/api/user", userRoutes);
